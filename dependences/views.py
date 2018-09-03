03fedb2bab97675ba731bc7fb8148b7d7fd0bf6a
jhonjaner11 <jhonjaner11@gmail.com>
2018-09-02 23:04:00 -0500

    formas

--- dependences/views.py
+++ dependences/views.py
@@ -14,9 +14,17 @@ def new_dependence(request):
     return render(request,'dependences/dependence_form.html',{'form':form})
 
 def list_dependences(request):
+    
     dependences = Dependence.objects.all()
     context = {'dependences': dependences}
+    form = DependenceForm()
+
+
+
     return render(request, 'dependences/dependence_list2.html',context)
+
+
+
 def search_dependences(request):
     try:
         dependences = Dependence.objects.filter(name__icontains=request.POST['key_word'])
@@ -25,12 +33,15 @@ def search_dependences(request):
     except Dependence.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'dependences/dependence_list2.html',context)
+
+
 def delete_dependence(request, id_dependence):
     dependence = Dependence.objects.get(id=id_dependence)
     if request.method == 'POST':
         dependence.delete()
         return redirect('dependences:list')
     return render(request, 'dependences/dependence_delete.html',{'dependence':dependence})
+
 def edit_dependence(request, id_dependence):
     dependence = Dependence.objects.get(pk=id_dependence)
     if request.method == 'GET':
